afb7f692d96172152f22b4db6810d5893040c0db
Daniel Kamar <kamarster@gmail.com>
2019-10-28 02:10:17 +0300

    reverse words in a string

---
+++ reversewordsinastring.py
@@ -0,0 +1,21 @@
+# s = input() \
+s = "i like this program very much"
+
+print()
+# METHOD 1
+
+words = s.split(' ')
+string = []
+for word in words:
+    string.insert(0, word)
+print("Reversed String:") 
+print(" ".join(string)) 
+
+
+print()
+# METHOD 2
+
+print
+print("Reversed String 2:") 
+print(' '.join(list(s.split(' '))[::-1]))
+
