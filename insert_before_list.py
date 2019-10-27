f68a29b85d7138c7da5244fc85789b0d4a7a0582
Daniel Kamar <kamarster@gmail.com>
2019-10-28 02:03:58 +0300

    insert before a list

---
+++ insert_before_list.py
@@ -0,0 +1,17 @@
+# Python3 program for Insertion in a list   
+# before any element using insert() method  
+  
+list1 = [ 1, 2, 3, 4, 5, 6 ] 
+  
+# Element to be inserted  
+element = 13 
+  
+# Element to be inserted before 3 
+beforeElement = 3 
+  
+# Find index 
+index = list1.index(beforeElement)  
+  
+# Insert element at beforeElement  
+list1.insert(index, element)  
+print(list1)
