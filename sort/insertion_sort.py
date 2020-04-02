e0deffe9d730bdfaba7393374f9dcc014858a4bf
Daniel Kamar <kamarster@gmail.com>
2020-04-02 19:53:20 +0300

    a clearer implementation of insertion sort

--- sort/insertion_sort.py
+++ sort/insertion_sort.py
@@ -2,18 +2,32 @@
 def insertionSort(arr): 
   
     # Traverse through 1 to len(arr) 
-    for i in range(1, len(arr)): 
-  
-        key = arr[i] 
-  
+    # for i in range(1, len(arr)): 
+    for index in range(1, len(arr)):
+
+        # key = arr[i] 
+        value = arr[index]
+
         # Move elements of arr[0..i-1], that are 
         # greater than key, to one position ahead 
         # of their current position 
-        j = i-1
-        while j >=0 and key < arr[j] : 
-                arr[j+1] = arr[j] 
-                j -= 1
-        arr[j+1] = key 
+        
+        # j = i-1
+        i = index - 1
+
+        # while j >=0 and key < arr[j] : 
+        #         arr[j+1] = arr[j] 
+        #         j -= 1
+        # arr[j+1] = key 
+
+        while i >=0:
+            if value < arr[i]:
+                arr[i+1] = arr[i]
+                arr[i] = value
+                i = i-1
+            else:
+                break
+
   
   
 # Driver code to test above 
