260a6f6144d960241c74e47f94f3ea612a3fa8d3
Daniel Kamar <kamarster@gmail.com>
2020-05-11 07:07:35 +0300

    Refactorings on reverse string

--- attempted algo n interview qs/reversewordsinastring.py
+++ attempted algo n interview qs/reversewordsinastring.py
@@ -8,17 +8,27 @@ words = s.split(' ')
 string = []
 for word in words:
     string.insert(0, word)
-print("Reversed String:") 
+print("Reversed String 1:") 
 print(" ".join(string)) 
 
 
 print()
 # METHOD 2
-
-print
 print("Reversed String 2:") 
 
 h = ' '.join(s.split(' ')[::-1])
-
 print(h)
 
+print()
+# METHOD 3
+print("Reversed String 3:")
+## initializing the string
+string = "I am a python programmer"
+## splitting the string on space
+words = string.split()
+# print(words)
+## reversing the words using reversed() function
+words = list(reversed(words))
+# print(words)
+## joining the words and printing
+print(" ".join(words))
